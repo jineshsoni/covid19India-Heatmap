a598df1f81bcff394a8e8939afaa34c7358ba833
Jinesh Soni <jinesh2025@gmail.com>
2020-04-09 16:35:26 +0530

    Data update : autoCommit: 20200409.163526

--- autopush.bat
+++ autopush.bat
@@ -1,6 +1,7 @@
 @echo off 
+wget https://jineshsoni-heatmapapi.glitch.me/covid19india.geojson -OutFile covid19india.geojson
 git add --all
-git commit -m "autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
+git commit -m "Data update : autoCommit: %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
 git push
 exit
 echo "Done!"
